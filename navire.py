eb3eac54c0e3ea12f593d4e4cd760991a93664bc
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2025-10-20 13:26:16 +0200

    feat: ajout de la classe Navire

---
+++ navire.py
@@ -0,0 +1,22 @@
+class navire():
+    def __init__(self,nom,positions):
+        # Attribut
+        self.nom = nom
+        self.positions = positions
+    
+        # 
+    def navire_touche(self,guess):
+        return guess in self.positions
+
+    def tirer(self,guess):
+        if guess in self.positions:
+            self.positions.remove(guess)
+            if not self.positions:
+                print(f"la navire {self.nom} est coulé !")
+            else:
+                print(f"Touché sur le {self.nom} !")
+            return True
+        return False
+    
+    def navire_coule(self):
+        return len(self.positions) == 0
