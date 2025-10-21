6ef5c984723f19ee6e5bffc775d6fde6e757789c
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2025-10-21 13:29:37 +0200

    refactor doc string

--- navire.py
+++ navire.py
@@ -6,9 +6,19 @@ class Navire():
     
         # 
     def navire_touche(self,guess):
+        """revoie True si la case correspond à une partie du navire"""
         return guess in self.positions
 
     def tirer(self,guess):
+        """
+        Marque la case comme touchée si elle correspond à une partie du navire.
+        
+        guess : coordonnée du tir
+        Si la case touche le navire :
+            - Supprime cette position de la liste des positions restantes
+            - Affiche un message :
+        Retourne True si le tir touche le navire, False sinon.
+        """
         if guess in self.positions:
             self.positions.remove(guess)
             if not self.positions:
@@ -19,4 +29,5 @@ class Navire():
         return False
     
     def navire_coule(self):
+        """renvoie True si toutes les positions du navire sont détruites"""
         return len(self.positions) == 0
